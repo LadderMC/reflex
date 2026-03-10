dc277c5630aa3ee9b948627a96d2ee588551fc0b
baptistesaul <baptistesaul.pro@gmail.com>
2026-03-10 23:32:32 +0100

    feat: upgrade Java 25 and change releasr version with new system

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("java")
     id("idea")
-    id("fr.ladder.releasr") version "1.0.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("fr.ladder.releasr") version "0.1.0"
+    id("com.gradleup.shadow") version "9.3.2"
 }
 
 group = "fr.ladder"
@@ -20,7 +20,14 @@ dependencies {
     implementation("org.slf4j", "slf4j-simple", "1.6.1")
 
     compileOnly("fr.snowtyy", "papermc", "1.8.8")
+
     implementation("io.github.classgraph", "classgraph", "4.8.181")
+}
+
+releasr {
+    url = "https://repo.lylaw.fr/repository/maven-releases/"
+    username = findProperty("REPO_USER") as String? ?: System.getenv("repoUser")
+    password = findProperty("REPO_PASSWORD") as String? ?: System.getenv("repoPassword")
 }
 
 tasks.shadowJar {
